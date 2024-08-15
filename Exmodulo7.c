e8cfb5580ef05d04209e1f51dccd855d10044ef3
Lya <lyabezzi@gmail.com>
2024-08-15 13:05:35 -0300

    opção de saída
    Incluso em sistema a opção de saída para o usuário.

--- Exmodulo7.c
+++ Exmodulo7.c
@@ -143,6 +143,7 @@ int main()
         printf("\t1- Registrar momes\n\n"); //Coletando informação de usuário
         printf("\t2- Consultar nomes\n\n"); //Coletando informação de usuário
         printf("\t3- Deletar nomes\n\n"); //Coletando informação de usuário
+        printf("\t4- Sair do sistema\n\n"); //Coletando informação de usuário
         printf("Opção: "); //Final do menu
   
         scanf("%d" , &opcao); //Armazenando a escolha do usuário
@@ -163,8 +164,13 @@ int main()
            deletar(); //Chamada de funções 
            break;
            
+           case 4: 
+           printf("Obrigado por utilizar nosso sistema/n"); //Chamada de funções
+           return 0;
+           break;
+           
            default:
-           printf("Opção não disponível\n"); 
+           printf("Opção não disponível\n"); //Chamada de funções
            system("pause"); //Responsável por pausar o sistema       
            break;
          
